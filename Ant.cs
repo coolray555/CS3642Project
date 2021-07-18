8a3c8ee6dce4d2e1fd24813ea7b61baff29ed771
Alex Jarvis <alexdjarvis@gmail.com>
2021-07-18 07:54:14 -0400

    Added funtions CurrentCIty, CanMove, and beginning logic for Move and ChooseNextCity functions

--- Ant.cs
+++ Ant.cs
@@ -5,17 +5,46 @@ namespace CS3642Project
 {
     public class Ant
     {
+        static Random rand = new Random();
         public List<City> VisitedNodes;
         public List<City> UnvisitedNodes;
         public int StartNodeID;
+        public int CurrentNodeID;
         public double Distance;
+        public bool IsElite;
+        public Cities world;
+        public int Beta;
 
-        public Ant(int startNodeID, List<City> Cities)
+        public Ant(int startNodeID, Cities cities, int Beta)
         {
-            StartNodeID = startNodeID;
-            Distance = 0;
-            VisitedNodes.Add(Cities.Where(x => x.Id == startNodeId).First());
-            UnvisitedNodes = Cities.Where(x => x.Id != startNodeId);
+            IsElite = rand.NextDouble() < .2;   //20 percent chance for elitist ant
+            this.world = cities;
+            this.StartNodeID = startNodeID;
+            this.Distance = 0;
+            VisitedNodes.Add(world.cities.Where(x => x.Id == startNodeId).First());
+            UnvisitedNodes = world.cities.Where(x => x.Id != startNodeId);
+        }
+        public bool CanMove(){
+            return VisitedNodes.Count == world?.cities.Count;
+        }
+
+        public City CurrentCity(){
+            return VisitedNodes[VisitedNodes.Count - 1];
+        }
+
+        public void Move(){     //TODO 
+            if(CanMove()){
+                City next = ChooseNextCity();
+                Distance += CurrentCity().calcDist(next);
+                VisitedNodes.Add(next);
+                UnvisitedNodes.Remove(next);
+            }
+        }
+
+        private City ChooseNextCity(){  //will choose next city base on whether the city is in range. Elitist ants will more strongly favor cities that are closer.
+            foreach(var city in UnvisitedNodes){
+
+            }
         }
     }
 }
